77e3116c2ebe373c313d04c7dac9fc51d30ba562
517tianhe <912385574@qq.com>
2016-03-14 15:05:04 +0800

    Initial Commit

---
+++ TYDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYDemo
+//
+//  Created by wanglj on 16/3/14.
+//  Copyright © 2016年 TH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYDemo
+//
+//  Created by wanglj on 16/3/14.
+//  Copyright © 2016年 TH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
