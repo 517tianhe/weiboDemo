ce20354e5ccee05eaad03119871d9b22109b9599
517tianhe <912385574@qq.com>
2016-03-15 16:32:33 +0800

    添加 微博管理类

---
+++ TYDemo/weiboManage/weiboManage.h
@@ -0,0 +1,13 @@
+//
+//  weiboManage.h
+//  TYDemo
+//
+//  Created by wanglj on 16/3/15.
+//  Copyright © 2016年 TH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface weiboManage : NSObject
+
+@end
---
+++ TYDemo/weiboManage/weiboManage.m
@@ -0,0 +1,13 @@
+//
+//  weiboManage.m
+//  TYDemo
+//
+//  Created by wanglj on 16/3/15.
+//  Copyright © 2016年 TH. All rights reserved.
+//
+
+#import "weiboManage.h"
+
+@implementation weiboManage
+
+@end
